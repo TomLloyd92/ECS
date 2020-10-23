eb10e6ddd78ab400270b2142d93de9bc4ef09fc1
Tom Lloyd <c00231308@itcarlow.ie>
2020-10-23 14:10:17 +0100

    Initial set up

---
+++ Project1/Project1/Gameplay.cpp
@@ -0,0 +1,30 @@
+#include "Gameplay.h"
+
+
+GamePlay::GamePlay()
+{
+
+}
+
+GamePlay::~GamePlay()
+{
+}
+
+void GamePlay::update(sf::Time t_deltaTime)
+{
+
+}
+
+void GamePlay::render(sf::RenderWindow& t_window)
+{
+}
+
+void GamePlay::setup(sf::Font& t_font)
+{
+
+
+}
+
+void GamePlay::initialise()
+{
+}
---
+++ Project1/Project1/Globals.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef GLOBALS
+#define GLOBALS
+
+enum class  GameMode
+{
+	Gameplay
+};
+
+
+const static int SCREEN_WIDTH = 10;
+const static int SCREEN_HEIGHT = 10;
+const static int MS_PER_UPDATE = 10;
+
+#endif
