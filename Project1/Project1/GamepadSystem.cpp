a782f62938d26d5e37901f8467f4ae134023987f
Tom Lloyd <c00231308@itcarlow.ie>
2020-10-28 22:05:00 +0000

    Controller added

--- Project1/Project1/GamepadSystem.cpp
+++ Project1/Project1/GamepadSystem.cpp
@@ -8,8 +8,6 @@ void GamepadSystem::update()
 	{
 		std::vector<Component> coms = it->getComponents();
 
-		std::cout << it->getName() << " is ";
-
 		//Lamda to find the GamePad component
 		auto comp_match_find = [](Component com)
 		{
@@ -18,13 +16,32 @@ void GamepadSystem::update()
 		auto AIComponentIter = std::find_if(begin(coms), end(coms), comp_match_find);
 		if (AIComponentIter == end(coms))
 		{
+			std::cout << it->getName() << " is ";
 			//Return Error if it is not found
 			std::cout << "NOT UPDATING GAMEPAD AS IT HAS NO GAMEPAD COMPONENT" << std::endl;
 		}
 		else
 		{
-			//Output updating
-			std::cout << "Updating GAMEPAD" << std::endl;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			{
+				std::cout << it->getName() << " is ";
+				std::cout << "Jumping" << std::endl;
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			{
+				std::cout << it->getName() << " is ";
+				std::cout << "Moving Left" << std::endl;
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			{
+				std::cout << it->getName() << " is ";
+				std::cout << "Moving Right" << std::endl;
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			{
+				std::cout << it->getName() << " is ";
+				std::cout << "Crouching" << std::endl;
+			}
 		}
 	}
 }
